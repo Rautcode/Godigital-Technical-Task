ee83e9415e9c5ca4ea498e488028afbfee628155
Manav Raut <133261591+Rautcode@users.noreply.github.com>
2025-03-05 14:13:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 # Ensure the S3 bucket exists before creating a new one
 data "aws_s3_bucket" "existing_bucket" {
   bucket = var.s3_bucket_name
+  count  = length(try([aws_s3_bucket.data_bucket[0].id], [])) == 0 ? 1 : 0
 }
 
 resource "random_string" "suffix" {
@@ -14,10 +15,18 @@ resource "random_string" "suffix" {
 }
 
 resource "aws_s3_bucket" "data_bucket" {
-  bucket = coalesce(data.aws_s3_bucket.existing_bucket.id, "${var.s3_bucket_name}-${random_string.suffix.result}")
+  count  = length(try([data.aws_s3_bucket.existing_bucket[0].id], [])) == 0 ? 1 : 0
+  bucket = "${var.s3_bucket_name}-${random_string.suffix.result}"
+}
+
+# Ensure the RDS subnet group exists before creating a new one
+data "aws_db_subnet_group" "existing_rds_subnet_group" {
+  name  = "data-pipeline-subnet-group"
+  count = length(try([aws_db_subnet_group.rds_subnet_group[0].id], [])) == 0 ? 1 : 0
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
+  count      = length(try([data.aws_db_subnet_group.existing_rds_subnet_group[0].id], [])) == 0 ? 1 : 0
   name       = "data-pipeline-subnet-group"
   subnet_ids = ["subnet-028121377f9e02a1d", "subnet-02b05146f9d302061"]  # Using correct subnets from AWS
 
@@ -28,7 +37,6 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 
 resource "aws_subnet" "subnet1" {
   vpc_id            = "vpc-00f29f899d9c9dc8d"  # Ensuring correct VPC
-  subnet_id         = "subnet-028121377f9e02a1d"
   cidr_block        = "10.0.1.0/24"
   availability_zone = "${var.aws_region}a"
 
@@ -39,7 +47,6 @@ resource "aws_subnet" "subnet1" {
 
 resource "aws_subnet" "subnet2" {
   vpc_id            = "vpc-00f29f899d9c9dc8d"  # Ensuring correct VPC
-  subnet_id         = "subnet-02b05146f9d302061"
   cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.aws_region}b"
 
